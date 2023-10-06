33ea0685f83162bf7f79b0422807083bd8be1432
tawfik-software <tawfik.ayade@yahoo.com>
2023-10-06 22:17:10 +0200

    Recommended-section button all product

--- src/Recommended/Recommended.js
+++ src/Recommended/Recommended.js
@@ -1,9 +1,18 @@
 import "./Recommended.css"
 
 function Recommended() {
-  return (
-    <div>Recommended</div>
-  )
+  return <>
+    <div>
+      <h2 className="recommended-flex">Recommended</h2>
+      <div className="recommended-flex">
+        <button className="btns">All Product</button>
+        <button className="btns">Nike</button>
+        <button className="btns">Adidas</button>
+        <button className="btns">Air Jordan</button>
+        <button className="btns">Puma</button>
+      </div>
+    </div>
+  </>
 }
 
 export default Recommended; 
