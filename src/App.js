2ee262bbbe447827c721392770181ab5d3b4d5eb
TeksaCorporate <k.djillah@teksacorporate.com>
2023-10-06 15:53:07 +0200

    Ajout du module App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Navigation  from './Navigation/Nav';
 import Products from './Products/Products';
 import Recommended from './Recommended/Recommended';
 
-
 function App() {
   return (
     <>
