42447b7d79dd4c33550c7c748e492f6eba891011
TeksaCorporate <k.djillah@teksacorporate.com>
2023-10-07 19:45:20 +0200

    Features de la partie  et de la partie

--- src/Sidebar/Category/Category.js
+++ src/Sidebar/Category/Category.js
@@ -1,16 +1,43 @@
-import Inputs from "../../components/Inputs";
+// import Inputs from "../../components/Inputs";
+import Input from "../../components/Inputs";
 import "./Category.css"; 
 
-function Category() {
+function Category({handleChange}) {
   return <div>
     <h2 className="sidebar-title">Category</h2>
 
-    <div>
-      <Inputs />
-      <Inputs />
-      <Inputs />
-      <Inputs />
-    </div>
+      <div>
+        <label className="sidebar-label-container">
+          <input onChange={handleChange} type="radio" value="" name="test" />
+          <span className="checkmark"></span>All
+        </label>
+
+        < Input 
+          handleChange={handleChange}
+          value="sneakers"
+          title="Sneakers"
+          name="test"
+        />
+        < Input 
+          handleChange={handleChange}
+          value="falts"
+          title="Falts"
+          name="test"
+        />
+        < Input 
+          handleChange={handleChange}
+          value="sandals"
+          title="Sandals"
+          name="test"
+        />
+        < Input 
+          handleChange={handleChange}
+          value="heels"
+          title="Heels"
+          name="test"
+        />
+
+      </div>
   </div>
 }
 
